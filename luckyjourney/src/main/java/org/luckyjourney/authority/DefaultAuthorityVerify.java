bd1dc39e29562254e4a0bec2155d2207c664cfe5
lyj138184 <138184138@qq.com>
2025-07-12 13:36:54 +0800

    Initial commit

---
+++ luckyjourney/src/main/java/org/luckyjourney/authority/DefaultAuthorityVerify.java
@@ -0,0 +1,15 @@
+package org.luckyjourney.authority;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Author: Xhy
+ * CreateTime: 2022-06-13 12:44
+ */
+public class DefaultAuthorityVerify implements AuthorityVerify{
+
+    @Override
+    public Boolean authorityVerify(HttpServletRequest request,String[] permissions) {
+        return true;
+    }
+}
